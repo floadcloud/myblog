e1068ed23e8d542022c2b0d08ae0e93874ae2e82
floadcloud <luohuilong1010@163.com>
2017-05-01 22:45:29 +0800

    add spring restful

--- src/main/java/com/floadcloud/myblog/controller/CategoryController.java
+++ src/main/java/com/floadcloud/myblog/controller/CategoryController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -20,12 +21,12 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
-    @RequestMapping(value = "list",method = RequestMethod.GET)
-    private String list(Model model){
+    @RequestMapping(value = "/list",method = RequestMethod.GET)
+    public @ResponseBody List<Category> list(){
 
         List<Category> list = categoryService.getList();
-        model.addAttribute("list",list);
-        return "list";
+        //model.addAttribute("list",list);
+        return list;
     }
 
 }
